1048560ae0d04042d7d482e8be52c92259bed6cc
thomas-binga <thomas.binga@gmail.com>
2022-12-07 09:55:07 +0100

    Sheepdog movement is now more smooth

--- classes/sheepdog.py
+++ classes/sheepdog.py
@@ -5,8 +5,7 @@ class Sheepdog(Entity):
 
     def simulate(self, event):
         x_mouse, y_mouse = event.x, event.y
-        unit_x, unit_y = self.unit(x_mouse - self.x, y_mouse - self.y)
-        self.move(10*unit_x, 10*unit_y)
+        self.moveto(x_mouse, y_mouse)
         # self.canvas.bind('<B1-Motion>', self.simulate)
         self.canvas.unbind('<B1-Motion>')
         self.canvas.after(100, lambda : self.canvas.bind('<B1-Motion>', self.simulate))
